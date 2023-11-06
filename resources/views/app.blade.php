57ab9b2c2ddd5f8f55555d934675bbc62aed73d3
adzinpratama <hilmiadzinpratama@gmail.com>
2023-11-06 13:31:33 +0700

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sakai Vue</title>
+    <link id="theme-css" rel="stylesheet" type="text/css" href="/themes/lara-light-indigo/theme.css">
+    @vite('resources/js/app.js')
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
